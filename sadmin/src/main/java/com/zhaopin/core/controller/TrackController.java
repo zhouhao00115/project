0f7b39e5eb19807b39680ece806e0819b3f199fa
zhouhao_jia@126.com <www000000>
2017-06-28 22:28:32 +0800

    customer信息列表页完成

---
+++ sadmin/src/main/java/com/zhaopin/core/controller/TrackController.java
@@ -0,0 +1,11 @@
+package com.zhaopin.core.controller;
+
+import org.springframework.stereotype.Controller;
+
+/**
+ * Created by Administrator on 2017/6/28.
+ */
+@Controller
+public class TrackController {
+
+}
