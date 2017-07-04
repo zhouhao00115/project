e7e6646cbfd9b422268dbb2c91c9efa102fe7e3d
zhou.hao <zhou.hao@zhaopin.com.cn>
2017-07-04 19:28:38 +0800

    报表数据定时任务

---
+++ sadmin/src/main/java/com/zhaopin/core/dao/ReportDao.java
@@ -0,0 +1,7 @@
+package com.zhaopin.core.dao;
+
+/**
+ * Created by zhou.hao on 2017/7/4.
+ */
+public interface ReportDao {
+}
---
+++ sadmin/src/main/java/com/zhaopin/core/dto/report/Code.java
@@ -0,0 +1,25 @@
+package com.zhaopin.core.dto.report;
+
+/**
+ * Created by zhou.hao on 2017/7/4.
+ */
+public class Code {
+    private String key;
+    private String valuse;
+
+    public String getKey() {
+        return key;
+    }
+
+    public void setKey(String key) {
+        this.key = key;
+    }
+
+    public String getValuse() {
+        return valuse;
+    }
+
+    public void setValuse(String valuse) {
+        this.valuse = valuse;
+    }
+}
